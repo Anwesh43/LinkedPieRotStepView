c84cf44cb2d3f67f4d06a5a54e908141500b8774
Anwesh43 <anweshthecool0@gmail.com>
2019-02-10 11:18:08 +0530

    adding PieRotStepView class

--- pierotstepview/src/main/java/com/anwesh/uiprojects/pierotstepview/PieRotStepView.kt
+++ pierotstepview/src/main/java/com/anwesh/uiprojects/pierotstepview/PieRotStepView.kt
@@ -54,3 +54,21 @@ fun Canvas.drawPRSNode(i : Int, scale : Float, paint : Paint) {
     }
     restore()
 }
+
+class PieRotStepView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
