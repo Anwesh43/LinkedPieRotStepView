1290a23d37966718c71ef9b1fea67cf2c68bada1
Anwesh43 <anweshthecool0@gmail.com>
2019-02-10 12:35:35 +0530

    adding PRSNode class

--- pierotstepview/src/main/java/com/anwesh/uiprojects/pierotstepview/PieRotStepView.kt
+++ pierotstepview/src/main/java/com/anwesh/uiprojects/pierotstepview/PieRotStepView.kt
@@ -119,4 +119,48 @@ class PieRotStepView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class PRSNode(var i : Int, val state : State = State()) {
+
+        private var next : PRSNode? = null
+        private var prev : PRSNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = PRSNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawPRSNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : PRSNode {
+            var curr : PRSNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this 
+        }
+    }
 }
