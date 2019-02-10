4e84d32e7cecd9ce2983d84225903b40d687a6c6
Anwesh43 <anweshthecool0@gmail.com>
2019-02-10 13:08:20 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedpiestepview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedpiestepview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedpiestepview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.pierotstepview.PieRotStepView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        PieRotStepView.create(this)
     }
 }
